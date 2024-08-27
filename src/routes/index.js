466f8263ac12f06cfee6ba69745193eefbcac8dd
JulianaAmoasei <late.parrot@gmail.com>
2024-08-27 11:31:09 -0300

    servidor de pé

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const routes = (app) => {
+  app.route("/").get((req, res) => res.status(200).send("Rota inicial"));
+  app.use(express.json());
+};
+
+export default routes;
